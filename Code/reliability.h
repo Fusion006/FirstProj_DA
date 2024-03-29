e8848c2d4017c888361529547cc72e70f1870d89
Pedro Oliveira <up202206498@up.pt>
2024-03-29 00:15:39 +0000

    Remove station tested

--- Code/reliability.h
+++ Code/reliability.h
@@ -6,10 +6,16 @@
 #include <set>
 #include <string>
 #include <map>
+double getCityDeficit(string cityName, const vector<pair<City*, double>>& cityToDeficit);
+map<string,double> getReservoirMap(Graph &g);
 
-void remReservoirController(Graph& g);
+void remReservoir(Graph& g);
 Reservoir* getReservoirInput(Graph &g);
 bool checkRelevantVertex(Graph &g, Vertex* v);
 void restoreReservoir(Graph &g, const map<string,int>& capacities);
 void printDiferences(Graph& g, const map<string,int>& start,map<string,int> end);
+
+void remStation(Graph& g);
+void restoreStations(Graph& g, const map<string,vector<Pipe*>>& stationsPipes);
+Station* getStationInput(Graph &g);
 #endif
